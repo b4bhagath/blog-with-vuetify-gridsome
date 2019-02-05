bc88b8d14257fd9b9a32de5054d777b3c4870646
b4bhagath <nikhilbhagath95@gmail.com>
2019-02-05 16:28:45 +0530

    gridsome update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vuetify from 'vuetify'
 import DefaultLayout from '~/layouts/Default.vue'
 
 import 'vuetify/dist/vuetify.min.css'
-import '@fortawesome/fontawesome-free/css/all.css'
+// import '@fortawesome/fontawesome-free/css/all.css'
 
 export default function (Vue) {
   Vue.use(Vuetify)
